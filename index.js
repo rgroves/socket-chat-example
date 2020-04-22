e413f6707be58ca5c4bdaa8666b0b44abba13400
Robert Groves <rwgdev@gmail.com>
2020-04-21 23:47:49 -0500

    Add jQuery and handle chat form submit message send.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
+  socket.on("chat message", (msg) => {
+    console.log(`message ${msg}`);
+  });
 });
 
 http.listen(3000, () => {
